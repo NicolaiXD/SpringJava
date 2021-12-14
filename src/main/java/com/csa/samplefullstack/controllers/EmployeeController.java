0bccb7899306891f9092c26f9b5cb56e14410139
NicolaiXD <82084148+NicolaiXD@users.noreply.github.com>
2021-12-14 21:50:36 +0800

    Initial Commit
    Initial Commit 12/14/2021 2150H

--- src/main/java/com/csa/samplefullstack/controllers/EmployeeController.java
+++ src/main/java/com/csa/samplefullstack/controllers/EmployeeController.java
@@ -3,8 +3,17 @@ package com.csa.samplefullstack.controllers;
 import com.csa.samplefullstack.entity.Employee;
 import com.csa.samplefullstack.repository.EmployeeRepository;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin; 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +24,31 @@ public class EmployeeController {
     
     @Autowired //spring - dependency injection
     private EmployeeRepository employeeRepository;
-    
-    
-    @RequestMapping("/employees")
+
+    @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
+    
+    @PostMapping("/employees")
+    public Employee postEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
+    }
+    
+    @GetMapping("/employees/{Id}")
+    public Employee getEmployeeFromId(@PathVariable Long id) {
+        return employeeRepository.findById(id).get();
+    }
+    
+    @PutMapping("/employees")
+    public Employee putEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
+    }
+    
+    @DeleteMapping("/employees/{employeeId}")
+    public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable Long employeeId) {
+        employeeRepository.deleteById(employeeId);
+        return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
+    }
+
 }
